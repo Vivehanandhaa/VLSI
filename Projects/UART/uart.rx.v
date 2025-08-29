63af3a5edaadc895fd13c8b1aaec731493e6de5b
Vivehanandhaa <114880752+Vivehanandhaa@users.noreply.github.com>
2025-08-29 15:53:55 +0530

    Update uart.rx.v

--- Projects/UART/uart.rx.v
+++ Projects/UART/uart.rx.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.08.2025 15:36:50
-// Design Name: 
-// Module Name: uart_rx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module uart_rx(clk,in,reset,parity,busy,data);
   input in, clk, reset; 
   output reg [9:0]data;
